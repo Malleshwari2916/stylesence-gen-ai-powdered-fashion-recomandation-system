0724fc0176286cc46abf2e8d3d5e8882d9121a04
Malleshwari2916 <237y1a6226@mlritm.ac.in>
2026-01-31 13:22:14 +0530

    Delete logic.py

--- logic.py
+++
@@ -1,7 +0,0 @@
-def product_recommendation(skin_tone):
-    products = {
-        "Fair": ["Pastel shirts", "Light blue jeans"],
-        "Medium": ["Earth tone jackets", "Olive shirts"],
-        "Dark": ["Bright colors", "White outfits"]
-    }
-    return products.get(skin_tone, [])
